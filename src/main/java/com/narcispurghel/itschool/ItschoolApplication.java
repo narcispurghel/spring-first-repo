a3dc1d12f0f28336af5997426a5249615d022d26
Narcis Purghel <narcispurghel@gmail.com>
2025-03-20 19:29:01 +0200

    Add Spring Boot Project"

---
+++ src/main/java/com/narcispurghel/itschool/ItschoolApplication.java
@@ -0,0 +1,13 @@
+package com.narcispurghel.itschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/narcispurghel/itschool/ItschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narcispurghel.itschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
